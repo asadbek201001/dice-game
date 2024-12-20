399f1ea1bc43ff2d9a3d4749c6c9c31309a1bbfc
AsadbekSWE <asadbekgulomov2010@gmail.com>
2024-12-20 18:06:37 +0500

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 document.getElementById("refresh").onclick = function() {
-
     var dice = document.querySelectorAll(".dicee");
     var player1Value = 0;
     for (let i = 0; i < dice.length; i++) {
@@ -7,8 +6,6 @@ document.getElementById("refresh").onclick = function() {
         dice[i].src = `./images/dice${randomNum}.png`;
         player1Value = randomNum; 
     }
-
-
     var dice2 = document.querySelectorAll(".dicee2");
     var player2Value = 0;
     for (let i = 0; i < dice2.length; i++) {
@@ -16,25 +13,30 @@ document.getElementById("refresh").onclick = function() {
         dice2[i].src = `./images/dice${randomNum2}.png`;  
         player2Value = randomNum2; 
     }
-
-  
     var title = document.getElementsByClassName("click")[0];
     if (player1Value > player2Value) {
         if (title) { 
-            title.textContent = "Player 1 Win!"; 
+            title.textContent = "🚩Player 1 Win!"; 
         }
     }
     else if (player1Value < player2Value) {
         if (title) { 
-            title.textContent = "Player 2 Win!"; 
+            title.textContent = "Player 2 Win! 🚩"; 
         }
     }
     else {
         if (title) { 
-            title.textContent = "Draw!"; 
+            title.textContent = "🚩Draw! 🚩"; 
         }
     }
 };
+
+
+
+
+
+
+
 
 
 
